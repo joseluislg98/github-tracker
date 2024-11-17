6c3e149401fe4f51da600d7fde6b930279d43d77
Jose Luis <jllg159753@gmail.com>
2024-11-16 19:36:04 -0500

    Add sample main.go router

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("Received POST request!")
+
+	defer r.Body.Close()
+
+	body, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Println("Error reading the request")
+
+		return
+	}
+
+	fmt.Printf(string(body))
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/hello", postHandler).Methods("POST")
+
+	fmt.Println("Server listening on post 8080")
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
